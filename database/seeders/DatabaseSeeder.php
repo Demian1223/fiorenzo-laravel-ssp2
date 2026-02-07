8e06632b7a42208b3cd412f2ba6b108d8c94ce21
Demian1223 <demiandesilva47@gmail.com>
2026-02-07 18:05:51 +0530

    Include all seeders in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,8 @@ class DatabaseSeeder extends Seeder
             CategorySeeder::class,
             BrandSeeder::class,
             ProductSeeder::class,
+            GenderCategorySeeder::class,
+            SlugPopulationSeeder::class,
         ]);
     }
 }
